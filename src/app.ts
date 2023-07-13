c6005dba91adca540ec32b2925995a688be839df
Leonardo Anjos <leodrums11@gmail.com>
2023-07-12 23:53:06 -0300

    e2e test and refactor lesson service

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
 import express from 'express'
+import { PrismaLessonRepository } from './repositories/prisma/PrismaLessonRepository';
+import { CreateLesson } from './services/CreateLesson';
 
 export const app = express()
 
 app.use(express.json())
 
-app.post('/lessons', (request, response) => {
-  return response.status(201).send()
+app.post('/lessons', async (request, response) => {
+  const { title, description } = request.body;
+
+  // TO BE DONE: transform to factory - Factory Pattern
+  const prismaLessonRepository = new PrismaLessonRepository()
+  const createLesson = new CreateLesson(prismaLessonRepository)
+
+  try {
+    await createLesson.execute({ title, description });
+    return response.status(201).send()
+  } catch (error: any) {
+    return response.status(400).json({ error: error.message })
+  }
+
 })
